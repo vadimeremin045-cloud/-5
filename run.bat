541484bd7060124b585cd210fe7bd9b401b24eb9
vadimeremin045-cloud <vadimeremin045@gmail.com>
2026-06-16 14:31:33 +0300

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,19 @@
+{
+  "name": "octagon-telegram-commands",
+  "version": "1.0.0",
+  "description": "Telegram bot for Octagon task with help, site and creator commands",
+  "main": "bot.js",
+  "author": "Vadim Eremin",
+  "license": "ISC",
+  "scripts": {
+    "start": "node bot.js",
+    "dev": "nodemon bot.js"
+  },
+  "dependencies": {
+    "dotenv": "^16.4.7",
+    "node-telegram-bot-api": "^0.66.0"
+  },
+  "devDependencies": {
+    "nodemon": "^3.1.9"
+  }
+}
